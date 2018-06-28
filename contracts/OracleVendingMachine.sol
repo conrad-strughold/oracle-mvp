ed0cfb3725bd694e040c0d687dfb91fc6dceb2b8
JG Carvalho <jgcarvalho93@gmail.com>
2018-06-28 20:38:04 -0300

    fixed proxy scheme

--- contracts/OracleVendingMachine.sol
+++ contracts/OracleVendingMachine.sol
@@ -1,84 +1,62 @@
 pragma solidity ^0.4.24;
-//import "../Oracles/Oracle.sol";
+import "../Oracles/Oracle.sol";
 import "../Oracles/CentralizedOracle.sol";
 import "../Utils/Proxy.sol";
 import "../Tokens/Token.sol";
 
+contract OracleVendingMachineData is Proxied {
 
-contract OracleVendingMachineInterface {
+  /*
+   *  Events
+   */
+  event OracleCreation(address indexed creator, Oracle centralizedOracle, bytes ipfsHash);
 
-    function buyOracle(bytes _ipfsHash) public returns (Oracle oracle);
-    function upgrade(address _masterCopy) public;
-    function changeFee(uint _fee) public;
-    function upgradeOracle(address _oracleMasterCopy) public;
-    function changeToken(address _token) public;
+  /*
+   *  Storage
+   */
+  address public owner;
+  uint public fee;
+  Oracle o racleMasterCopy;
+  Token public paymentToken;
 
 }
 
-contract OracleVendingMachineData {
+// BASIC PROXY - ISN'T UPGRADABLE YET
+contract OracleVendingMachineProxy is Proxy, OracleVendingMachineData{
 
-    /*
-     *  Events
-     */
-    event OracleCreation(address indexed creator, Oracle centralizedOracle, bytes ipfsHash);
+  contructor(address _proxied) Proxy(_proxied){
+    owner = msg.sender;
+  }
 
-    /*
-     *  Storage
-     */
-    address public owner;
-    uint public fee;
-    address public oracleMasterCopy;
-    Token public paymentToken;
-
-    /*
-     *  Modifiers
-     */
-    modifier isOwner () {
-        // Only owner is allowed to proceed
-        require(msg.sender == owner);
-        _;
-    }
-
-    /*
-     *  Basic permanent functions
-     */
-    function upgrade(address _masterCopy) public isOwner {
-        masterCopy = _masterCopy;
-    }
-
-    function changeFee(uint _fee) public isOwner {
-        fee = _fee;
-    }
-
-    function upgradeOracle(address _oracleMasterCopy) public isOwner {
-        oracleMasterCopy = _oracleMasterCopy;
-    }
-
-    function changePaymentToken(address _paymentToken) public isOwner {
-        paymentToken = _paymentToken;
-    }
 }
 
-contract OracleVendingMachine is Proxied, OracleVendingMachineData {
-    /// @dev Constructor sets owner address and IPFS hash
-    /// @param _ipfsHash Hash identifying off chain event description
-    constructor(address proxied, address _owner, Token _paymentToken, bytes _fee, address _oracleMasterCopy)
-        public
-        Proxy(proxied)
-    {
-        owner = _owner;
-        fee = _fee;
-        oracleMasterCopy = _oracleMasterCopy;
-        paymentToken = _paymentToken;
-    }
-}
+contract OracleVendingMachine is OracleVendingMachineData {
 
-contract OracleVendingMachineProxy is Proxy, OracleVendingMachineInterface, OracleVendingMachineData {
+  /*
+   *  Modifiers
+   */
+  modifier isOwner () {
+      // Only owner is allowed to proceed
+      require(msg.sender == owner);
+      _;
+  }
 
-    function buyOracle(bytes _ipfsHash) public returns (Oracle oracle){
-        require(token.transferFrom(msg.sender, owner, fee));
-        oracle = CentralizedOracle(new CentralizedOracleProxy(oracleMasterCopy, owner, _ipfsHash));
-        emit OracleCreation(msg.sender, oracle, _ipfsHash);
-    }
+  function changeFee(uint _fee) public isOwner {
+      fee = _fee;
+  }
+
+  function upgradeOracle(address _oracleMasterCopy) public isOwner {
+      oracleMasterCopy = _oracleMasterCopy;
+  }
+
+  function changePaymentToken(address _paymentToken) public isOwner {
+      paymentToken = _paymentToken;
+  }
+
+  function buyOracle(bytes _ipfsHash) public returns (Oracle oracle){
+      require(token.transferFrom(msg.sender, owner, fee));
+      oracle = CentralizedOracle(new CentralizedOracleProxy(oracleMasterCopy, owner, _ipfsHash));
+      emit OracleCreation(msg.sender, oracle, _ipfsHash);
+  }
 
 }
