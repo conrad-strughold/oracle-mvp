fe760c2e0c60303e28496361f6c0c90ab46e9697
JG Carvalho <jgcarvalho93@gmail.com>
2018-07-06 10:53:31 -0300

    Basic vending machine

---
+++ contracts/CentralizedBugOracle.sol
@@ -0,0 +1,93 @@
+pragma solidity ^0.4.24;
+
+import "./Gnosis/Oracles/Oracle.sol";
+
+contract CentralizedBugOracle is Oracle{
+
+  /*
+   *  Storage
+   */
+  address public owner;
+  bytes public ipfsHash;
+  bool public isSet;
+  int public outcome;
+  uint256 public fee;
+  uint256 public fundingPeriod;
+  address public maker;
+  address public taker;
+  bool public ready;
+
+  mapping(address => uint256) funding;
+  mapping(address => uint256) desiredOutcome;
+
+  /*
+   *  Modifiers
+   */
+  modifier isOwner () {
+      // Only owner is allowed to proceed
+      require(msg.sender == owner);
+      _;
+  }
+
+  /// @dev Funds the contract with tokens to pay fo rrulling
+  function fund(address _side, uint256 _amount){
+    funding[_side] += amount;
+  }
+
+  /// Puts the contract in a state to give rulling
+  function setReady(){
+    require(funding[maker] >= fee);
+    require(funding[taker] >= fee);
+    ready = true;
+  }
+
+  //Gives a rulling in favor of maker  if taker doesn not fund the contract in appropriate time
+  function walkoverRulling() {
+    if(now > fundingPeriod){
+      require(funding[maker] > fee);
+      _setOutcome(desiredOutcome(maker));
+    }
+  }
+
+  /// @dev Sets event outcome
+  /// @param _outcome Event outcome
+  function setOutcome(int _outcome)
+      public
+      isOwner
+  {
+      // Result is not set yet
+      require(!isSet);
+      require(ready);
+      _setOutcome(_outcome);
+  }
+
+  /// @dev Returns if winning outcome is set
+  /// @return Is outcome set?
+  function isOutcomeSet()
+      public
+      view
+      returns (bool)
+  {
+      return isSet;
+  }
+
+  /// @dev Returns outcome
+  /// @return Outcome
+  function getOutcome()
+      public
+      view
+      returns (int)
+  {
+      return outcome;
+  }
+
+
+  //@dev internal funcion to set the outcome sat
+  function _setOutcome(int _outcome) internal {
+    isSet = true;
+    outcome = _outcome;
+    emit OutcomeAssignment(_outcome);
+  }
+
+
+}
--- contracts/OracleVendingMachine.sol
+++ contracts/OracleVendingMachine.sol
@@ -3,11 +3,28 @@ pragma solidity ^0.4.24;
 import "./Gnosis/Oracles/Oracle.sol";
 import "./Gnosis/Oracles/CentralizedOracle.sol";
 import "./Gnosis/Utils/Proxy.sol";
+import "./Gnosis/Utils/Math.sol";
 import "./Gnosis/Tokens/Token.sol";
 import "./OracleVendingMachineProxy.sol";
 
 //Vending machine Logic goes in this contract
-contract OracleVendingMachine is OracleVendingMachineData {
+contract OracleVendingMachine {
+  using Math for uint256;
+
+  /*
+   *  Storage
+   */
+  address public owner;
+  uint public fee;
+  Oracle oracleMasterCopy;
+  Token public paymentToken;
+
+
+  mapping (address => uint256) public balances;
+  mapping (address => bool) public balanceChecked;
+  mapping (address => (address => uint256) public oracleIndexes;
+  mapping (address => (address => (uint256 => bool)) public oracleAccepted;
+  mapping (address => (address => (uint256 => bool)) public oracleDeployed;
 
   /*
    *  Modifiers
@@ -30,10 +47,44 @@ contract OracleVendingMachine is OracleVendingMachineData {
       paymentToken = Token(_paymentToken);
   }
 
-  function buyOracle(bytes _ipfsHash) public returns (Oracle oracle){
-      require(paymentToken.transferFrom(msg.sender, owner, fee));
-      oracle = CentralizedOracle(new CentralizedOracleProxy(oracleMasterCopy, owner, _ipfsHash));
-      emit OracleCreation(msg.sender, oracle, _ipfsHash);
+  function deployOracle(bytes _ipfsHash, address maker, address taker, uint256 oracleIndex) internal returns(Oracle oracle){
+    require(oracleAccepted[maker][taker][id]);
+    require(!oracleDeployed[maker][taker][id])
+    oracle = CentralizedOracle(new CentralizedOracleProxy(oracleMasterCopy, owner, _ipfsHash));
+    oracleDeployed[maker][taker][id] = true;
+    emit OracleCreation(msg.sender, oracle, _ipfsHash);
+  }
+
+  //POTENTIAL VULNERABILITY IN UNKONWING MAKER
+  function confirmOracle(address maker, uint index) public returns(bool) {
+    if(balanceChecked[msg.sender]) checkBalance();
+    require(balances[msg.sender] = balances[msg.sender].sub(fee));
+    require(!oracleAccepted[maker][msg.sender][index]);
+    oracleAccepted[maker][msg.sneder][index] = true;
+  }
+
+  function buyOracle(bytes _ipfsHash, address taker) public returns (uint index){
+    if(balanceChecked[msg.sender]) checkBalance();
+    require(balances[msg.sender] = balances[msg.sender].sub(fee));
+    index = oracleIndexes[msg.sender][taker];
+  }
+
+  function buyOracleFor(bytes _ipfsHash, address maker, address taker) public isOwner (){
+    if(balanceChecked[maker]) checkBalance();
+    if(balanceChecked[taker]) checkBalance();
+
+    require(balances[maker] = balances[maker].sub(fee));
+    require(balances[taker] = balances[taker].sub(fee));
+
+    oracleAccepted[maker][taker][oracleIndexes[maker][taker]] = true;
+    deployOracle(_ipfsHash,maker,taker,oracleIndex);
+    oracleIndexes[maker][taker] += 1;
+  }
+
+  function checkBalance(address holder) public {
+    require(!balanceChecked[holder]);
+    balances[holder] = paymentToken.balanceOf(holder);
+    balanceChecked[holder] = true;
   }
 
 }
