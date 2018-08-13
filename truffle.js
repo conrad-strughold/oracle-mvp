86631e020c5da9226790500ea480841b8b06a865
JG Carvalho <jgcarvalho93@gmail.com>
2018-08-13 16:39:32 -0300

    Testing fixe for travis

--- truffle.js
+++ truffle.js
@@ -19,10 +19,17 @@ module.exports = {
   // to customize your Truffle configuration!
   networks: {
     development: {
-      host: "localhost",
+      host: "127.0.0.1",
       port: 8545,
       gasPrice: 5000000000,
       network_id: "*"
-    }
+    },
+    coverage: {
+      host: "localhost",
+      network_id: "*",
+      port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
+      gas: 0xfffffffffff, // <-- Use this high gas value
+      gasPrice: 0x01      // <-- Use this low gas price
+    },
   }
 }
